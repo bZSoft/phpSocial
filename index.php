8b97c424f4cbbde7f5ad5c72a68b5d7ac4e2b56f
bZSoft <itsn3ff@live.com>
2014-04-04 16:21:47 -0400

    changes 6

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'main.i.php'; ?>
+
+</body>
+</html>
--- main.i.php
+++ main.i.php
@@ -1,3 +1,12 @@
-<?php include 'cnf.php'; 
+<?php include 'cnf.php'; ?>
 
-?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+<title><? echo $title; ?></title>
+<link rel="stylesheet" type="text/css" href="<? echo $cssc; ?>">
+</head>
+<body>
+
