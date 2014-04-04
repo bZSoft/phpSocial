923a366a8c327918bcc8d8bc845694e10618261e
bZSoft <itsn3ff@live.com>
2014-04-04 16:15:36 -0400

    changes 5

--- cnf.php
+++ cnf.php
@@ -1,4 +1,8 @@
 <?php
+$logo = "img/l.bmp";
+$cssc = "stl.css";
+$nfp = "no";
+$title = "phpSocial - social networking script";
 mysql_connect("localhost","root","");
 mysql_select_db(phpsocial);
 ?>
