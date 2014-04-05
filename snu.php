2d933f0c557d7083ba9ee94f645438550fd0f3f5
bZSoft <itsn3ff@live.com>
2014-04-04 21:39:04 -0400

    c 23

--- snu.php
+++ snu.php
@@ -1,13 +1,13 @@
 <?php include 'main.i.php'; ?>
 <form action="#" method="post">
-	<input name="fn" placeholder="First name">
-	<input name="ln" placeholder="Last name">
-	<input name="u" placeholder="Username">
-	<input name="p" placeholder="Password">
-	<input name="b" placeholder="Blurb">
-	<input name="pp" type="file">
-	<input name="fav" placeholder="Favorite things">
-	<input value="snu" name="snu">
+	<input name="fn" placeholder="First name"><br><br>
+	<input name="ln" placeholder="Last name"><br><br>
+	<input name="u" placeholder="Username"><br><br>
+	<input name="p" placeholder="Password"><br><br>
+	<input name="b" placeholder="Blurb"><br><br>
+	<input name="pp" type="file"><br><br>
+	<input name="fav" placeholder="Favorite things"><br><br>
+	<input value="snu" name="snu"><br>
 </form>
 </div>
 </body>
