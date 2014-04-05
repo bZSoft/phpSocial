79c3334b2e8016958034a46703bf49a411fa3940
bZSoft <itsn3ff@live.com>
2014-04-04 21:37:10 -0400

    c 22

--- snu.php
+++ snu.php
@@ -1,4 +1,14 @@
 <?php include 'main.i.php'; ?>
+<form action="#" method="post">
+	<input name="fn" placeholder="First name">
+	<input name="ln" placeholder="Last name">
+	<input name="u" placeholder="Username">
+	<input name="p" placeholder="Password">
+	<input name="b" placeholder="Blurb">
+	<input name="pp" type="file">
+	<input name="fav" placeholder="Favorite things">
+	<input value="snu" name="snu">
+</form>
 </div>
 </body>
 </html>
